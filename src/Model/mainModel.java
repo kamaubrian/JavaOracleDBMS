f5801b2eab6db671a847c82ff20c07ca62fcb8a1
mtotodev05 <mtotodev05@gmail.com>
2017-09-27 08:25:58 +0300

    Initial Commit

---
+++ src/Model/mainModel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.SQLException;
+import java.sql.*;
+
+/**
+ *
+ * @author brian-kamau
+ */
+public abstract class mainModel {
+    
+    private static final String dbDriver="oracle.jdbc.driver.OracleDriver";
+    private static final String dbUrl ="jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String dbUsername="hr";
+    private static final String dbPassword ="hr";
+    protected PreparedStatement pst =null;
+    protected Statement stat= null;
+    protected Connection conn = null;
+    protected ResultSet rst = null;
+    
+    
+    public final boolean dbConnect(){
+        boolean success = true;
+        try{
+            Class.forName(dbDriver);
+            conn = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+            
+        }catch(ClassNotFoundException | SQLException ex){
+            System.out.println("Oracle Connection Error"+ex.getMessage());
+            success = false;
+        }
+        return success;
+    }
+    public final boolean dbDisconnect(){
+        try{
+            if(rst!=null){
+                rst.close();
+            }
+            if(conn!=null){
+                conn.close();
+            }
+            if(stat!=null){
+                stat.close();
+            }
+            
+        }catch(Exception ex){
+            System.out.println("Disconnecting Exception"+ex.getMessage());
+        }
+                        
+        return true;
+        
+    }
+    
+    
+    
+}
---
+++ test/Model/mainModelTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author brian-kamau
+ */
+public class mainModelTest {
+    
+    public mainModelTest() {
+    }
+    
+    @Test
+    public void testDBConnection(){
+        System.out.println("Database Connection Tests");
+        mainModel instance = new mainModelimpl();
+        boolean expResult = true;
+        boolean result = instance.dbConnect();
+        assertEquals(expResult,result);
+        
+        
+    }   
+    public class mainModelimpl extends mainModel{
+        
+    }
+}
